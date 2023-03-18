069dd8076d0557dc1ee294cdab440c76bdf467e8
skcreationz <selvakumard274@gmail,com>
2023-03-18 22:31:55 +0530

    changes selva

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express=require('express');
 const app=express();
+const port=3000;
 //middleware
 app.use(express.json());
 app.set("view engine",'ejs');
 app.get("/",(req,res)=>[
     res.render("home")
 ])
-app.listen(3000,()=>{
+app.listen(process.env.PORT||port,()=>{
     console.log("server connected");
 })
