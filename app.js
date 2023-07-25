df28e266d1116187437f6b601601c3dc4824a13f
skcreationz <selvakumard274@gmail,com>
2023-07-25 14:53:25 +0530

    final updation

--- app.js
+++ app.js
@@ -3,15 +3,49 @@ const certificate_data =require( "./datas/certification.json");
 const homedatas=require("./datas/home_datas.json");
 const projects_data=require("./datas/projects.json");
 const path=require("path");
+const cors=require('cors');
+require('dotenv').config({path:"./config.env"});
+const {connectTodb,getDb} =require("./db");
+const mongoose=require('mongoose');
 const app=express();
-const port=3000;
+const port=3000||process.env.port;
+const db=require("./db")
+//DB connection
+
+connectTodb((err)=>{
+  if(!err){
+   
+    db=getDb();
+  }
+});
+////
+ 
+app.listen(port,()=>{
+    db.connectTodb(function(err){
+        if(err)
+            console.err(err);
+        })
+        console.log("server connected");
+ 
+});
+app.get("/my",(req,res)=>{
+     let books=[];
+     db.collection("beast").find()
+      .forEach(element => {
+        books.push(element);
+    }) .then(()=>{
+    res.status(200).json(books);
+ })
+  
+})
+
+
 //middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
 app.use("/datas",express.static('datas'));
-app.use('/stylesheet',express.static('stylesheet'))
-
+app.use('/stylesheet',express.static('stylesheet'));
 //view engine specs
 app.set("view engine",'ejs');
 
@@ -35,6 +69,3 @@ app.get("/about",(req,res)=>{
     res.render("about",{data:projects_data,Name:name,bio:bio});
 });
 //port specification
-app.listen(process.env.PORT||port,()=>{
-    console.log("server connected");
-});
